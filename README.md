4829cbfdb7a263d84126274231a363ab164e9c04
Jun <3010977181@qq.com>
2023-12-18 19:26:06 +0800

    1.0

---
+++ README.md
@@ -0,0 +1,12 @@
+# Blog
+基于poetize.cn博客修改的可直接部署的个人博客网站
+
+## 新增功能
+### 网盘系统
+### 笔记系统
+### ChatGPT
+
+## 项目源码
+### 作者:poetize
+### 网址:poetize.cn
+### gitee地址:https://gitee.com/littledokey/poetize-vue2
---
+++ poetize-im-vue3-master/src/utils/constant.js
@@ -0,0 +1,43 @@
+export default {
+  baseURL: "https://www.zhujunpeng.love/api", //把resone.cn改成自己的域名
+  webBaseURL: "https://www.zhujunpeng.love", //把resone.cn改成自己的域名
+  imURL: "https://www.zhujunpeng.love/im", //把resone.cn改成自己的域名
+  imBaseURL: "www.zhujunpeng.love", //把resone.cn改成自己的域名
+  webHistory: "/im/", //不动
+  wsProtocol: "wss", //不动
+  wsPort: "",//不动
+
+  // baseURL: "https://poetize.cn/api",
+  // webBaseURL: "https://poetize.cn",
+  // imURL: "https://poetize.cn/im",
+  // imBaseURL: "poetize.cn",
+  // webHistory: "/im/",
+  // wsProtocol: "wss",
+  // wsPort: "",
+
+  hitokoto: "https://v1.hitokoto.cn",
+  jinrishici: "https://v1.jinrishici.com/all.json",
+  jitang: "https://api.oick.cn/dutang/api.php",
+  shehui: "https://api.oick.cn/yulu/api.php",
+  yiyan: "https://api.oick.cn/yiyan/api.php",
+  dog: "https://api.oick.cn/dog/api.php",
+
+  //前后端定义的密钥，AES使用16位
+  cryptojs_key: "aoligeimeimaobin",
+
+  qiniuUrl: "https://upload-z2.qiniup.com",
+  qiniuDownload: "https://cdn.zhujunpeng.love/",
+
+  before_color_1: "black",
+  after_color_1: "linear-gradient(45deg, #f43f3b, #ec008c)",
+
+  before_color_2: "rgb(131, 123, 199)",
+  after_color_2: "linear-gradient(45deg, #f43f3b, #ec008c)",
+
+  tree_hole_color: ["#ee7752", "#e73c7e", "#23a6d5", "#23d5ab", "rgb(131, 123, 199)", "#23d5ab"],
+
+  userId: 1,
+  source: 0,
+
+  emojiList: ['衰', '鄙视', '再见', '捂嘴', '摸鱼', '奋斗', '白眼', '可怜', '皱眉', '鼓掌', '烦恼', '吐舌', '挖鼻', '委屈', '滑稽', '啊这', '生气', '害羞', '晕', '好色', '流泪', '吐血', '微笑', '酷', '坏笑', '吓', '大兵', '哭笑', '困', '呲牙']
+}
